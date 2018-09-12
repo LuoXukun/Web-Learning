08684bfa7fa3a596fe3b0045bf0ee7f393224709
Luo XuKun <861392914@qq.com>
2018-09-12 15:32:59 +0800

    文件系统

--- Node.js学习/文件系统/main3.js
+++ Node.js学习/文件系统/main3.js
@@ -1,1 +1,21 @@
-// 关闭文件
+// 其他操作
+
+var fs = require('fs');
+
+// 获取文件信息
+fs.stat();
+
+// 截取文件
+fs.ftruncate();
+
+// 删除文件
+fs.unlink();
+
+// 创建目录
+fs.mkdir();
+
+// 读取目录
+fs.readdir();
+
+// 删除目录
+fs.rmdir();
